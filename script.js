a60939b206192ae7993ccc2798fd532c9d39bb0c
sanjanarai123 <161812190+sanjanarai123@users.noreply.github.com>
2024-05-29 06:59:32 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function display(value){
+    document.getElementById("result").value += value;
+}
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+function calculate(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
